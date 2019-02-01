9904ffc29ed80d4796f23384a582a97ad725f1cd
Thomas Churchman <thomas@kepow.org>
2019-02-01 20:34:31 +0100

    Add state version.

--- home.nix
+++ home.nix
@@ -6,6 +6,8 @@ in
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
+    home.stateVersion = "18.09";
+
     imports = [
       (./systems + "/${system_name}.nix")
     ];
