c185ae5d3bea13b7692be560807893c0d7ae8aa2
Thomas Churchman <thomas@kepow.org>
2019-02-01 16:48:38 +0100

    Add some default programs.

--- home.nix
+++ home.nix
@@ -7,9 +7,42 @@ in
     programs.home-manager.enable = true;
 
     # Common packages.
-    home.packages = [
+    home.packages = with pkgs; [
       (import ./my-programs/volume-control)
       (import ./my-programs/thingshare)
+      # Dotfiles manager.
+      yadm
+      # The basics.
+      firefox
+      thunderbird
+      # Cloud.
+      seafile-client
+      # Tools.
+      direnv
+      libnotify
+      # Documents.
+      nomacs
+      okular
+      zathura
+      libreoffice
+      (
+        python3.withPackages (
+          python-packages: with python-packages; [
+            numpy
+          ]
+        )
+      )
+      (
+        texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-small
+            apacite
+            floatflt wrapfig # Figures
+            enumitem
+            courier # Font
+          ;
+        }
+      )
     ];
 
     imports = [
