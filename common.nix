0985ea571245aea3c829c64e4d6c8a4eeaea37f5
Thomas Churchman <thomas@kepow.org>
2019-04-20 23:34:42 +0200

    Explicitly use unstable / master nixpkgs.

--- common.nix
+++ common.nix
@@ -1,113 +1,116 @@
-{ pkgs, ... }:
+{ ... }:
 
-{
-  # Allow unfree software.
-  nixpkgs.config.allowUnfree = true;
+let
+  unstable = import <unstable> {};
+in
+  {
+    # Allow unfree software.
+    nixpkgs.config.allowUnfree = true;
 
-  # Common packages.
-  home.packages = with pkgs; [
-    (import ./my-programs/brightness-control)
-    (import ./my-programs/volume-control)
-    (import ./my-programs/thingshare)
-    (import ./my-programs/extract)
-    # Dotfiles manager.
-    yadm
-    # Editor.
-    ((emacsPackagesNgGen (emacs.override {
-      withGTK3 = true;
-      withGTK2 = false;
-    })).emacsWithPackages (epkgs:
-        (with epkgs.melpaPackages; [
-          use-package
-          evil
-          general
-          which-key
-          projectile
-          flycheck
-          flycheck-inline
-          flycheck-rust
-          zenburn-theme
-          rust-mode
-          cargo
-          racer
-          magma-mode
-          column-enforce-mode
-        ]) ++ (with epkgs.melpaStablePackages; [
-          fill-column-indicator
-          magit
-          smart-mode-line
-          monokai-theme
-          neotree
-          markdown-mode
-          nix-mode
-          haskell-mode
-          ess # R
-          ess-R-data-view
-          ivy
-          swiper
-          counsel
-          avy
-        ]) ++ (with epkgs.elpaPackages; [
-          org
-          beacon
-          auctex
-          company
-        ])
+    # Common packages.
+    home.packages = with unstable.pkgs; [
+      (import ./my-programs/brightness-control)
+      (import ./my-programs/volume-control)
+      (import ./my-programs/thingshare)
+      (import ./my-programs/extract)
+      # Dotfiles manager.
+      yadm
+      # Editor.
+      ((emacsPackagesNgGen (emacs.override {
+        withGTK3 = true;
+        withGTK2 = false;
+      })).emacsWithPackages (epkgs:
+          (with epkgs.melpaPackages; [
+            use-package
+            evil
+            general
+            which-key
+            projectile
+            flycheck
+            flycheck-inline
+            flycheck-rust
+            zenburn-theme
+            rust-mode
+            cargo
+            racer
+            magma-mode
+            column-enforce-mode
+          ]) ++ (with epkgs.melpaStablePackages; [
+            fill-column-indicator
+            magit
+            smart-mode-line
+            monokai-theme
+            neotree
+            markdown-mode
+            nix-mode
+            haskell-mode
+            ess # R
+            ess-R-data-view
+            ivy
+            swiper
+            counsel
+            avy
+          ]) ++ (with epkgs.elpaPackages; [
+            org
+            beacon
+            auctex
+            company
+          ])
+        )
       )
-    )
-    # The basics.
-    firefox
-    thunderbird
-    # Chat.
-    mydiscord
-    # Cloud.
-    seafile-client
-    # Tools.
-    direnv
-    libnotify
-    gnome3.gnome-system-monitor
-    filelight
-    ark
-    # Font tools.
-    gnome3.gucharmap
-    # Documents.
-    nomacs
-    okular
-    zathura
-    libreoffice
-    # Databases
-    dbeaver # Database GUI.
-    (
-      python3.withPackages (
-        python-packages: with python-packages; [
-          numpy
-        ]
+      # The basics.
+      firefox
+      thunderbird
+      # Chat.
+      mydiscord
+      # Cloud.
+      seafile-client
+      # Tools.
+      direnv
+      libnotify
+      gnome3.gnome-system-monitor
+      filelight
+      ark
+      # Font tools.
+      gnome3.gucharmap
+      # Documents.
+      nomacs
+      okular
+      zathura
+      libreoffice
+      # Databases
+      dbeaver # Database GUI.
+      (
+        python3.withPackages (
+          python-packages: with python-packages; [
+            numpy
+          ]
+        )
       )
-    )
-    (
-      texlive.combine {
-        inherit (pkgs.texlive)
-          amsmath # Math
-          amsfonts # Math
-          # psfonts # textcomp
-          cm-super
-          scheme-small
-          apacite
-          floatflt wrapfig # Figures
-          enumitem
-          courier # Font
-          hyperref
-          capt-of
-        ;
-      }
-    )
-  ];
+      (
+        texlive.combine {
+          inherit (pkgs.texlive)
+            amsmath # Math
+            amsfonts # Math
+            # psfonts # textcomp
+            cm-super
+            scheme-small
+            apacite
+            floatflt wrapfig # Figures
+            enumitem
+            courier # Font
+            hyperref
+            capt-of
+          ;
+        }
+      )
+    ];
 
-  imports = [
-    ./cfg/gtk.nix
-    ./cfg/xresources.nix
-    ./cfg/dunst.nix
-    ./cfg/polybar
-    ./cfg/redshift
-  ];
-}
+    imports = [
+      ./cfg/gtk.nix
+      ./cfg/xresources.nix
+      ./cfg/dunst.nix
+      ./cfg/polybar
+      ./cfg/redshift
+    ];
+  }
--- systems/castor.nix
+++ systems/castor.nix
@@ -1,23 +1,29 @@
 { lib, pkgs, ... }:
+let
+  master = import ./../nixpkgs {};
+in
+  {
+    imports = [
+      ../common.nix
+      ../cfg/xmonad
+    ];
 
-{
-  imports = [
-    ../common.nix
-    ../cfg/xmonad
-  ];
+    services.polybar = {
+      script = ''
+        MONITOR=DP-4 polybar top &
+        MONITOR=DVI-D-0 polybar top &
+      '';
+      config."module/wlan".interface = "wlp3s0";
+      config."bar/top".modules-right = "volume redshift wlan cpu memory date";
+    };
 
-  services.polybar = {
-    script = ''
-      MONITOR=DP-4 polybar top &
-      MONITOR=DVI-D-0 polybar top &
-    '';
-    config."module/wlan".interface = "wlp3s0";
-    config."bar/top".modules-right = "volume redshift wlan cpu memory date";
-  };
-
-  home.packages = with pkgs; [
-    # Games.
-    steam
-    steam-run
-  ];
-}
+    home.packages = (with pkgs;
+        []
+      ) ++ (with master.pkgs;
+        [
+          # Games.
+          steam
+          steam-run
+        ]
+      );
+  }
