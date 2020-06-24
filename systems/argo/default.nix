17dd68c21ff55c84c31548bb196f1a5fd78bf066
Thomas Churchman <thomas@kepow.org>
2020-06-24 02:47:34 +0200

    Add new system: argo.

---
+++ systems/argo/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ../../common.nix ../../cfg/bspwm/pollux.nix ../../cfg/email ];
+
+  services.polybar = {
+    script = ''
+      MONITOR=eDP-1-1 polybar top &
+    '';
+    config."module/wlan".interface = "wlp59s0";
+    config."bar/top".modules-right =
+      "wlan cpu memory battery date";
+  };
+
+  # Set some dpi scaling.
+  xresources.properties = { "Xft.dpi" = 96; };
+}
