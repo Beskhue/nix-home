d14d12894db2f35886fb56ac92f14dd9b6509d7c
Thomas Churchman <thomas@kepow.org>
2020-10-19 13:54:03 +0300

    Run nixfmt

--- systems/argo/default.nix
+++ systems/argo/default.nix
@@ -13,8 +13,7 @@
       MONITOR=eDP-1-1 polybar top &
     '';
     config."module/wlan".interface = "wlp59s0";
-    config."bar/top".modules-right =
-      "wlan cpu memory battery date";
+    config."bar/top".modules-right = "wlan cpu memory battery date";
   };
 
   # Set some dpi scaling.
