24e780b994f13807e308515790e85d9785830cde
Thomas Churchman <thomas@kepow.org>
2020-12-22 12:01:22 +0200

    daw: disable some things, change some channels

--- cfg/daw/default.nix
+++ cfg/daw/default.nix
@@ -1,24 +1,30 @@
 { pkgs, ... }:
 let
-  unstable = import <unstable> { };
-  master = import ../../nixpkgs { };
-in {
-  home.packages = with unstable; [
+  systemLibOverlay = self: super: {
+    libjack2 = pkgs.libjack2;
+    jack2 = pkgs.jack2;
+    # gtk2 = pkgs.gtk2;
+    # gtk3 = pkgs.gtk3;
+    # qt5 = pkgs.qt5;
+    # qt5.qtbase = pkgs.qt5.qtbase;
+    # # python2 = pkgs.python2;
+    # # python2Packages = pkgs.python2Packages;
+    # # python3 = pkgs.python3;
+    # # python3Packages = pkgs.python3Packages;
+  };
+  unstable = import <unstable> {
+    overlays = [ systemLibOverlay ];
+  };
+  master = import ../../nixpkgs {
+    overlays = [ systemLibOverlay ];
+  };
+in
+{
+  home.packages = (with pkgs; [
     # Generic LV2
     jalv
     lilv
-    # Plugin host
-    (carla.override {
-      python3Packages = pkgs.python3Packages;
-      qtbase = pkgs.qt5.qtbase;
-      wrapQtAppsHook = pkgs.qt5.wrapQtAppsHook;
-      gtk2 = pkgs.gtk2;
-      gtk3 = pkgs.gtk3;
-    })
-    # Collection
-    lsp-plugins
     # Synths
-    zynaddsubfx
     surge
     helm
     qsynth
@@ -34,20 +40,34 @@ in {
     supercollider
     ### DAW
     ardour
-  ]) ++ [
+    # Collection
+    lsp-plugins
+  ]) ++ (with unstable; [
+    # Plugin host
+    (carla.override {
+      # python3Packages = pkgs.python3Packages;
+      # qtbase = pkgs.qt5.qtbase;
+      # wrapQtAppsHook = pkgs.qt5.wrapQtAppsHook;
+      # gtk2 = pkgs.gtk2;
+      # gtk3 = pkgs.gtk3;
+    })
+  ]) ++ (with master; [
     # Wine VST bridge
+    # # (master.airwave.override {
+    # #   qt5 = pkgs.qt5;
+    # #   # libX11 = pkgs.libX11;
+    # #   wine = pkgs.wine;
+    # # })
     # (master.airwave.override {
     #   qt5 = pkgs.qt5;
-    #   # libX11 = pkgs.libX11;
-    #   wine = pkgs.wine;
     # })
-    (master.airwave.override {
-      qt5 = pkgs.qt5;
-    })
     # Use latest renoise, with stable libjack2 version.
-    (master.renoise.override {
-      libjack2 = pkgs.libjack2;
+    (renoise.override {
+      # libjack2 = pkgs.libjack2;
       releasePath = ~/music-production/renoise/rns_322_linux_x86_64.tar.gz;
     })
-  ];
+    (zynaddsubfx.override {
+      guiModule = "zest";
+    })
+  ]);
 }
