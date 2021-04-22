b3f01d8ea3758689dd6f4d3b4cd71657b9a21a50
Thomas Churchman <thomas@kepow.org>
2021-04-22 16:58:29 +0200

    daw: add Harrison AVA plugin derivation

---
+++ cfg/daw/harrison-ava.nix
@@ -0,0 +1,30 @@
+{ lib, stdenv, curl, gnutls, libglvnd, libX11, libXext, autoPatchelfHook }:
+stdenv.mkDerivation rec {
+  pname = "harrison-ava";
+  version = "2021-04-08";
+
+  src = ~/music-production/plugins/harrison-ava/vst;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    curl
+    gnutls
+    libglvnd
+    libX11
+    libXext
+  ];
+
+  installPhase = ''
+    mkdir -p $out/lib/vst
+    cp ./* $out/lib/vst
+  '';
+
+  preFixup = ''
+    for file in $out/lib/vst/*.so; do
+      patchelf --replace-needed libcurl-gnutls.so.4 libcurl.so.4 "$file"
+    done
+  '';
+}
